72b67ad8b9bc88794f62119e8e9bf4e95aa63381
Will-Conroy <william1conroy1@gmail.com>
2021-06-13 17:10:11 +0100

    Puppet Action Menu
    When Clicking an action memu for just one puppet opens and closes allother windows

--- Assets/Scripts/Game/TestButtons.cs
+++ Assets/Scripts/Game/TestButtons.cs
@@ -29,7 +29,7 @@ public class TestButtons : MonoBehaviour
         List<GameObject> targetsDeck = new List<GameObject> {GameObject.Find("DeckPrefab")};
         Command command = new DrawToHandSizeCommand(targetsDeck);
         _gameController.performAction(command);
-
     }
-    
+
+ 
 }
--- Assets/Scripts/Puppet/Ui/ActionMenu/ActionMenuController.cs
+++ Assets/Scripts/Puppet/Ui/ActionMenu/ActionMenuController.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class ActionMenuController : MonoBehaviour
 {
     private bool isVisable;
+    private GameObject[] puppets;
 
     public void Awake(){
+        puppets = GameObject.FindGameObjectsWithTag("Puppet");
+        foreach(GameObject puppet in puppets)
+            puppet.GetComponentInChildren<PuppetController>().PuppetMenuClick.AddListener(delegate{toggleVisiable(puppet.transform.GetChild(1).GetChild(0).gameObject);});
         isVisable = false;
         gameObject.SetActive(isVisable);
     }
 
-
-
-    public void toggleVisiable(){
-        isVisable = !isVisable;
-        gameObject.SetActive(isVisable);
+    public void toggleVisiable(GameObject me){
+        if(me == gameObject){
+            isVisable = !isVisable;
+            gameObject.SetActive(isVisable);
+        }else if(isVisable){
+            isVisable = !isVisable;
+            gameObject.SetActive(isVisable);
+        }
+        
     }
 }
