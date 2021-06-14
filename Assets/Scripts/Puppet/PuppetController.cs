fff9ccb40176994b868979f014421153d1339e53
Will-Conroy <william1conroy1@gmail.com>
2021-06-14 16:52:48 +0100

    Dont want to

--- Assets/Scripts/Puppet/PuppetController.cs
+++ Assets/Scripts/Puppet/PuppetController.cs
@@ -6,11 +6,38 @@ using UnityEngine.Events;
 public class PuppetController : MonoBehaviour
 {
     
+    
+    [SerializeField] private bool ownedByPlayerOne;
     /* Events */
     public UnityEvent PuppetMenuClick;
+     enum layerName
+        {
+            Attack = 6,
+            Defend = 7,
+        };
     
     void OnMouseDown()
     {
         PuppetMenuClick?.Invoke();
     }
+    public void Awake(){
+        if(ownedByPlayerOne){
+            gameObject.layer = (int) layerName.Attack;
+        }else{
+            gameObject.layer = (int) layerName.Defend;
+        }
+    }
+
+    public bool isOwenedByPlayerOne(){
+        return ownedByPlayerOne;
+    }
+
+    public void toggleLayor(){
+        if (gameObject.layer == (int) layerName.Attack){
+            gameObject.layer =  (int) layerName.Defend;
+        }else{
+             gameObject.layer =(int)  layerName.Attack;
+        }
+    }
+
 }
